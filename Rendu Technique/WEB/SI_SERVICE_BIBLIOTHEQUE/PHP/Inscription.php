dfbb40239f87368a82610495e0699be6d92df8f4
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-04-07 19:01:49 +0200

    Mise à jour du style pour le formulaire d'inscription

--- Rendu Technique/WEB/SI_SERVICE_BIBLIOTHEQUE/PHP/Inscription.php
+++ Rendu Technique/WEB/SI_SERVICE_BIBLIOTHEQUE/PHP/Inscription.php
@@ -6,7 +6,21 @@
 <!DOCTYPE html>
 <HTML>
 	<HEAD>
-		<style type="text/css"></style>
+		<style type="text/css">
+			.labelspan
+			{
+				display: inline-block;
+				align: left;
+				width: 50%;
+			}
+			.inputspan
+			{
+				display: inline-block;
+				align: right;
+				width: 50%;
+			}
+			/*Ca ne donne pas ce que je veux car inline-block fait se comporter le span comme un div ou un p, mais j'aime bien le résultat.*/
+		</style>
 		<link rel="stylesheet" type="text/css" href="../CSS/Index.CSS" charset="utf-8"/>
 		<link rel="shortcut icon" type="image/x-icon" href="../Contenus/images/cirno9.jpg"/>
 		<meta charset="utf-8"/>
@@ -20,14 +34,14 @@
 		<form id='inscription' name='inscription' method='POST' autocomplete="on" action="#">
 		<fieldset>
 			<LEGEND>Inscription : </LEGEND>
-			<div><label for="NomEmploye">Nom : </label>
-			<input type="text" name="NomEmploye" id="NomEmploye" required></input></div>
-			<div><label for="Adresse">Adresse : </label>
-			<input type="text" name="Adresse" id="Adresse"></input></div>
-			<div><label for="NumeroTelephone">Numéro de Téléphone : </label>
-			<input type="varchar" min="10" max="15" name="NumeroTelephone" id="NumeroTelephone"></input></div>
-			<div><label for="NomService">Service : </label>
-			<input type="text" name="NomService" id="NomService"></input></div>
+			<div><span class="labelspan"><label for="NomEmploye">Nom : </label></span>
+			<span class="inputspan"><input type="text" name="NomEmploye" id="NomEmploye" required></input></span></div>
+			<div><span class="labelspan"><label for="Adresse">Adresse : </label></span>
+			<span class="inputspan"><input type="text" name="Adresse" id="Adresse"></input></span></div>
+			<div><span class="labelspan"><label for="NumeroTelephone">Numéro de Téléphone : </label></span>
+			<span class="inputspan"><input type="varchar" min="10" max="15" name="NumeroTelephone" id="NumeroTelephone"></input></span></div>
+			<div><span class="labelspan"><label for="NomService">Service : </label></span>
+			<span class="inputspan"><input type="text" name="NomService" id="NomService"></input></span></div>
 			<br/>
 			<div><input type="submit" class="boutonsFormulaires" name="submit" value="Valider" style="left: 0%">
 				<input type="reset" class="boutonsFormulaires" name="reset" value="Réinitialiser" onclick="location.href='Inscription.php'" style="right: 0%"></div>
